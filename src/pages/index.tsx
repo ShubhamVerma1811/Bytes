5313c6e096584946f40c779852f75691de752ae2
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2021-06-16 11:07:12 +0530

    refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import { BoringAvatar, ImageCard, Pill, Title } from "components"
-import Harper from "db/harper/config"
-import { GridLayout, PageLayout } from "layouts"
-import { GetServerSideProps } from "next"
-import Link from "next/link"
-import { Fragment } from "react"
-import { classnames } from "tailwindcss-classnames"
-import { PostType } from "types/Post"
-import { TagType } from "types/Tag"
+import { Pill, PostCard, Title } from 'components'
+import Harper from 'db/harper/config'
+import { GridLayout, PageLayout } from 'layouts'
+import { GetServerSideProps } from 'next'
+import Link from 'next/link'
+import { Fragment } from 'react'
+import { classnames } from 'tailwindcss-classnames'
+import { PostType } from 'types/Post'
+import { TagType } from 'types/Tag'
 
 export default function Home({
   feedPosts,
@@ -15,100 +15,45 @@ export default function Home({
   feedPosts: Array<PostType>
   feedTags: Array<TagType>
 }) {
-  //#region
-
-  // const {
-  //   data: { feedPosts, feedTags },
-  //   error,
-  //   isLoading,
-  // } = useQuery("getFeed", async () => {
-  //   const harper = new Harper()
-  //   const feedPosts = await harper.post({
-  //     operation: "sql",
-  //     sql: "SELECT p.*,u.name,u.email FROM bytes.post AS p INNER JOIN bytes.user AS u ON p.uid = u.uid",
-  //   })
-
-  //   const feedTags = await harper.post({
-  //     operation: "sql",
-  //     sql: "SELECT * FROM bytes.tag",
-  //   })
-
-  //   return {
-  //     feedPosts,
-  //     feedTags,
-  //   }
-  // })
-
-  // if (isLoading) return null
-
-  //#endregion
-
   return (
     <PageLayout>
-      <div>
-        <div className={classnames("flex", "my-10", "justify-center")}>
-          <div>
-            <div className={classnames("py-3")}>
-              <Title title='Trending Tags' />
-            </div>
-            <div className={classnames("flex", "flex-wrap")}>
-              {feedTags?.map(({ name, tid, color }) => {
-                return (
-                  <Fragment key={tid}>
-                    <Link href={`/tag/${name}`}>
-                      <div>
-                        <Pill name={name} color={color} />
-                      </div>
-                    </Link>
-                  </Fragment>
-                )
-              })}
-            </div>
-          </div>
-        </div>
-
+      <div className={classnames('flex', 'my-10', 'justify-center')}>
         <div>
-          <div className={classnames("flex", "my-5", "justify-center")}>
-            <div className={classnames("py-3")}>
-              <Title title='Trending Bytes' />
-            </div>
+          <div className={classnames('py-3')}>
+            <Title title='Trending Tags' />
           </div>
-
-          <GridLayout>
-            {/* @ts-ignore */}
-            {feedPosts?.map(({ pid, title, name, images, slug }) => {
+          <div className={classnames('flex', 'flex-wrap')}>
+            {feedTags?.map(({ name, tid, color }) => {
               return (
-                <div key={pid}>
-                  <Link href={`byte/${slug}`}>
+                <Fragment key={tid}>
+                  <Link href={`/tag/${name}`}>
                     <div>
-                      <div className={classnames("py-2")}>
-                        <p className='text-2xl font-medium'>{title}</p>
-                        <div
-                          className={classnames(
-                            "flex",
-                            "justify-between",
-                            "w-max"
-                          )}>
-                          <div>
-                            <BoringAvatar
-                              name={name}
-                              size={30}
-                              variant='beam'
-                            />
-                          </div>
-                          <div className={classnames("mx-2")}>
-                            <p>By {name}</p>
-                          </div>
-                        </div>
-                      </div>
-                      <ImageCard src={images[0]} />
+                      <Pill name={name} color={color} />
                     </div>
                   </Link>
-                </div>
+                </Fragment>
               )
             })}
-          </GridLayout>
+          </div>
         </div>
+      </div>
+
+      <div>
+        <div className={classnames('flex', 'my-5', 'justify-center')}>
+          <div className={classnames('py-3')}>
+            <Title title='Trending Bytes' />
+          </div>
+        </div>
+
+        <GridLayout>
+          {feedPosts?.map((post) => {
+            return (
+              <Fragment key={post.pid}>
+                <PostCard post={post} />
+              </Fragment>
+            )
+          })}
+        </GridLayout>
       </div>
     </PageLayout>
   )
@@ -117,13 +62,13 @@ export default function Home({
 export const getServerSideProps: GetServerSideProps = async () => {
   const harper = new Harper()
   const feedPosts = await harper.post({
-    operation: "sql",
-    sql: "SELECT p.*,u.name FROM bytes.post AS p INNER JOIN bytes.user AS u ON p.uid = u.uid",
+    operation: 'sql',
+    sql: 'SELECT p.*,u.name FROM bytes.post AS p INNER JOIN bytes.user AS u ON p.uid = u.uid LIMIT 6',
   })
 
   const feedTags = await harper.post({
-    operation: "sql",
-    sql: "SELECT * FROM bytes.tag",
+    operation: 'sql',
+    sql: 'SELECT * FROM bytes.tag LIMIT 5',
   })
 
   return {
