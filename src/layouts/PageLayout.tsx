9c9df3e763a4335885395d914ad135f8300c57b4
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2021-06-16 10:27:56 +0530

    prettier

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -1,13 +1,13 @@
-import React from "react"
-import { classnames } from "tailwindcss-classnames"
-import Footer from "../components/Footer/Footer"
-import Navbar from "../components/Navbar/Navbar"
+import React from 'react'
+import { classnames } from 'tailwindcss-classnames'
+import Footer from '../components/Footer/Footer'
+import Navbar from '../components/Navbar/Navbar'
 
 export const PageLayout = ({ children }) => {
   return (
     <div>
       <Navbar />
-      <div className={classnames("md:mx-4")}>{children}</div>
+      <div className={classnames('md:mx-4')}>{children}</div>
       <Footer />
     </div>
   )
