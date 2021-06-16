1fc732a2a47f24a2b263931d4299b0c10c429f12
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2021-06-16 11:06:49 +0530

    added .envs

--- next.config.js
+++ next.config.js
@@ -3,5 +3,12 @@ module.exports = {
   env: {
     HARPER_BYTES_URI: process.env.HARPER_BYTES_URI,
     HARPER_BYTES_BASIC: process.env.HARPER_BYTES_BASIC,
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
   },
+  images: { domains: ['firebasestorage.googleapis.com'] },
 }
