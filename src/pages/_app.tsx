202bb0cd9bce33d3665160d663ca677e06b1e23b
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2021-06-16 11:07:37 +0530

    prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,18 @@
-import { UserContext } from "context/UserContext"
-import firebase from "db/firebase/config"
-import { AppProps } from "next/app"
-import dynamic from "next/dynamic"
-import "nprogress/nprogress.css"
-import React, { useEffect, useState } from "react"
-import { QueryClient, QueryClientProvider } from "react-query"
-import { getAuthUserFromHarper } from "services/harperRequests"
-import "styles/tailwind.css"
+import { TwScreenDebug } from 'components'
+import { UserContext } from 'context/UserContext'
+import firebase from 'db/firebase/config'
+import { AppProps } from 'next/app'
+import dynamic from 'next/dynamic'
+import Head from 'next/head'
+import 'nprogress/nprogress.css'
+import React, { useEffect, useState } from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { getAuthUserFromHarper } from 'services/harperRequests'
+import 'styles/tailwind.css'
 
 const TopProgressBar = dynamic(
   () => {
-    return import("components/TopProgressBar/TopProgressBar")
+    return import('components/TopProgressBar/TopProgressBar')
   },
   { ssr: false }
 )
@@ -33,9 +35,13 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <React.Fragment>
+      {process.env.NODE_ENV === 'development' && <TwScreenDebug />}
       <TopProgressBar />
       <QueryClientProvider client={queryClient}>
         <UserContext.Provider value={{ user, setUser }}>
+          <Head>
+            <title>Bytes | Bit Sized Learining</title>
+          </Head>
           <Component {...pageProps} />
         </UserContext.Provider>
       </QueryClientProvider>
