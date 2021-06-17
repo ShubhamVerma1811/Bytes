18005d73577c09e2172932dd28d33d8323a5cbff
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2021-06-17 15:57:56 +0530

    React icon heart

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { AiFillHeart } from 'react-icons/ai'
 
 export const Navbar = () => {
   return (
     <footer className='max-w-6xl m-auto text-center py-4'>
-      Made with ❤ by{' '}
+      Made with <AiFillHeart className='inline' style={{ color: 'red' }} /> by{' '}
       <a
         href='https://shubhamverma.me'
         target='_blank'
