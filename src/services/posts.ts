88096c91ac71ede967b034495b515814b62f461d
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2021-06-17 13:07:44 +0530

    new updateReaction function

--- src/services/posts.ts
+++ src/services/posts.ts
@@ -1,15 +1,17 @@
 import Harper from 'db/harper/config'
+import { PostType } from 'types/Post'
+import { TagType } from 'types/Tag'
 
 const harper = new Harper()
 
 export const getPost = async (pslug: string) => {
-  const post = await harper.post({
+  const post: PostType[] = await harper.post({
     operation: 'sql',
     sql: `SELECT p.*,u.name FROM bytes.post AS p INNER JOIN bytes.user AS u ON u.uid=p.uid WHERE p.slug='${pslug}'`,
   })
 
   if (post.length) {
-    const tags = await harper.post({
+    const tags: TagType[] = await harper.post({
       operation: 'sql',
       sql: `SELECT t.* FROM bytes.post_tag AS pt INNER JOIN bytes.tag AS t ON pt.tid=t.tid WHERE pt.pid='${post[0]?.pid}'`,
     })
@@ -28,8 +30,28 @@ export const getPost = async (pslug: string) => {
         post: null,
         tags: null,
         message: "Post doesn't exist",
-        result: null,
+        results: null,
       },
     }
   }
+}
+
+export const updatePostReaction = async (postSlug: string, count: number) => {
+  const {
+    props: {
+      post: { reactions, pid },
+    },
+  } = await getPost(postSlug)
+
+  await harper.post({
+    operation: 'update',
+    schema: 'bytes',
+    table: 'post',
+    records: [
+      {
+        pid,
+        reactions: reactions + count,
+      },
+    ],
+  })
 }
