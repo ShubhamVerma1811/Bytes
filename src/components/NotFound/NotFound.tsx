6930c5cd37eb3cce23309884fe5ba419fbbb2f52
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2021-06-17 13:53:09 +0530

    update comp to take in title

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -5,11 +5,17 @@ import React from 'react'
 import Head from 'next/head'
 import { classnames } from 'tailwindcss-classnames'
 
-export const NotFound = ({ message }) => {
+export const NotFound = ({
+  message,
+  title,
+}: {
+  message: string
+  title: string
+}) => {
   return (
     <PageLayout>
       <Head>
-        <title>404 Not Found</title>
+        <title>{title}</title>
       </Head>
       <Title title={message} />
       <Link href='/'>
